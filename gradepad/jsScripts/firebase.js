df6e12c6050e54b25581b913408bb4310127d11a
Chris Jackson <2chrisjackson@gmail.com>
2025-10-28 10:56:30 -0400

    changing hero when signed in

--- gradepad/jsScripts/firebase.js
+++ gradepad/jsScripts/firebase.js
@@ -3,7 +3,7 @@
 // 2) Ensure Google provider is enabled in Firebase Console
 
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js';
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js';
 import { firebaseConfig } from './firebase-config.js';
 
 // moved to gitignored firebase-config.js (see firebase-config.example.js)
@@ -15,6 +15,10 @@ const provider = new GoogleAuthProvider();
 export async function signInWithGoogle() {
   const result = await signInWithPopup(auth, provider);
   return result.user;
+}
+
+export function onAuthChange(callback) {
+  return onAuthStateChanged(auth, callback);
 }
 
 
