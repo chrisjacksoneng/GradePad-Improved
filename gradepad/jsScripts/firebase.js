45c3f26b3ff7e2cfe76bdf72b6b3f31f9cc2eebc
Chris Jackson <2chrisjackson@gmail.com>
2025-10-28 10:20:48 -0400

    adding gitignore

--- gradepad/jsScripts/firebase.js
+++ gradepad/jsScripts/firebase.js
@@ -4,16 +4,9 @@
 
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js';
+import { firebaseConfig } from './firebase-config.js';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBnI1jmsEnYLsZizowBBGUu0ZRpblM5bH8",
-  authDomain: "gradepad-improved.firebaseapp.com",
-  projectId: "gradepad-improved",
-  storageBucket: "gradepad-improved.firebasestorage.app",
-  messagingSenderId: "223375674663",
-  appId: "1:223375674663:web:6a2c54d972034e1c21ed02",
-  measurementId: "G-7T6TTG6XQL"
-};
+// moved to gitignored firebase-config.js (see firebase-config.example.js)
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
